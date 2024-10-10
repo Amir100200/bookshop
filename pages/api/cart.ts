d16ef3b72a2a176e354a5f3d17e8c49308247dab
Amir <amirmohammad20203030@gmail.com>
2024-10-10 08:23:21 +0330

    Update

---
+++ pages/api/cart.ts
@@ -0,0 +1,19 @@
+// This is an example of how to access a session from an API route
+import type { NextApiRequest, NextApiResponse } from "next"
+
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+  
+  let body = JSON.parse(req.body)
+  
+  await db.collection("cart").deleteMany({})
+
+  await db.collection("cart").insertMany(body.map(b=>{
+    return {title:b}
+  }))
+
+ await db.collection("cart").deleteMany({})
+  res.send({ code: 0,})
+}
